99501aab6c6c031f8e97d4db945006a2ddff9180
oliv1008 <omillochau@gmail.com>
2018-12-03 17:55:24 +0100

    fzkuef

--- gestion/ManagementScreen/EventScreen/EventScreen.gd
+++ gestion/ManagementScreen/EventScreen/EventScreen.gd
@@ -4,6 +4,8 @@ onready var tie = $MainContainer/tie
 onready var choiceLabel = $MainContainer/DialogueContainer/Choice
 onready var choice1Button = $MainContainer/DialogueContainer/ChoiceButtonsContainer/Choice1
 onready var choice2Button = $MainContainer/DialogueContainer/ChoiceButtonsContainer/Choice2
+onready var choice3Button = $MainContainer/DialogueContainer/ChoiceButtonsContainer2/Choice3
+onready var choice4Button = $MainContainer/DialogueContainer/ChoiceButtonsContainer2/Choice4
 
 func _ready():
 	tie.connect("input_enter", self, "_on_input_enter")
@@ -13,6 +15,11 @@ func _ready():
 	tie.connect("resume_break", self, "_on_resume_break")
 	tie.connect("tag_buff", self, "_on_tag_buff")
 
+	tie.buff_text("Ceci est un test\nCeci est un autre test", 0.1)
+	tie.buff_silence(1)
+	tie.buff_text("\n\nCeci est toujours un test")
+	tie.set_state(tie.STATE_OUTPUT)
+	
 #func _process(delta):
 #	# Called every frame. Delta is time since last frame.
 #	# Update game logic here.
@@ -23,8 +30,12 @@ func _on_input_enter(s):
 	pass
 
 func _on_buff_end():
-	print("Buff End")
-	pass
+	choiceLabel.text = "Votre choix ?"
+	choice1Button.text = "Choix 1"
+	choice2Button.text = "Choix 2"
+	choiceLabel.visible = true
+	choice1Button.visible = true
+	choice2Button.visible = true
 
 func _on_state_change(i):
 	print("New state: ", i)
