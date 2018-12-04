970e155f78adae5be8734317f83821d343dc69aa
oliv1008 <omillochau@gmail.com>
2018-12-04 03:52:14 +0100

    html

--- gestion/IntroMenu/IntroMenu.gd
+++ gestion/IntroMenu/IntroMenu.gd
@@ -3,6 +3,8 @@ extends Control
 var pathToManagementScreen = "res://gestion/ManagementScreen/MainManagementScreen/MainManagementScreen.tscn"
 
 func _ready():
+	if OS.get_name() == "HTML5":
+		OS.set_window_maximized(true)
 	MusicPlayer.get_node("MusicStream").volume_db = 0
 	MusicPlayer.brutalPlaySong("MainTheme")
 
